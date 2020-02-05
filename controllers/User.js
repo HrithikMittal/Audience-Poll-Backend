d17687147f1fef7db0901e997680badc2f92eaec
HrithikMittal <adhikanshmittal@gmail.com>
2020-02-06 01:28:24 +0530

    Add jsonwebtoken

--- controllers/User.js
+++ controllers/User.js
@@ -1,3 +1,6 @@
+const expressJwt = require("express-jwt");
+const jwt = require("jsonwebtoken");
+
 const User = require("../modals/User");
 
 const signup = (req, res) => {
@@ -19,16 +22,26 @@ const signin = (req, res) => {
       if (!retuser) {
         return res.json({ message: "User not found" });
       }
-      if (retuser.password === user.password) {
-        res.json({ message: "User is login successfully" });
+      if (retuser.password !== user.password) {
+        res.json({ message: "Password is wrong" });
       }
+      const token = jwt.sign({ _id: retuser._id }, process.env.JWT_SECRET);
+      res.cookie("t", token, { expire: new Date() + 9999 });
+      const { _id, regno, name } = retuser;
+      return res.json({ token, user: { _id, regno, name } });
     })
     .catch(err => {
       console.log("Error is ", err.message);
     });
 };
 
+const requireSignIn = expressJwt({
+  secret: process.env.JWT_SECRET,
+  userProperty: "auth"
+});
+
 module.exports = {
   signup,
-  signin
+  signin,
+  requireSignIn
 };
